8b6d3d58bcbf8dd6e968925b00317b7f5cf8dc72
Morten Brunbjerg Bech <bech@rephlex.dk>
2017-08-28 11:51:17 +0200

    First commit
    This is just the initial basic _s WordPress theme. It’ll change soon.

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * KrimiLeksikon Theme Customizer
+ *
+ * @package KrimiLeksikon
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function krimileksikon_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'krimileksikon_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'krimileksikon_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'krimileksikon_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function krimileksikon_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function krimileksikon_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function krimileksikon_customize_preview_js() {
+	wp_enqueue_script( 'krimileksikon-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'krimileksikon_customize_preview_js' );
