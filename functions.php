ffcbf4bbb0ea84020f700a282a376a88a5aec2fe
Morten Brunbjerg Bech <bech@rephlex.dk>
2017-08-31 13:26:15 +0200

    Adding Slick Slider with Bower and custom js files.

--- functions.php
+++ functions.php
@@ -123,6 +123,8 @@ function krimileksikon_scripts() {
 
 	wp_enqueue_script( 'krimileksikon-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'slick', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array( 'jquery' ), '20151215', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
