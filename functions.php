a22060ebac6418891ac1c1c7030c4787ea38862c
Morten Brunbjerg Bech <bech@rephlex.dk>
2017-08-31 13:47:35 +0200

    Enqueueing concatenated js.

--- functions.php
+++ functions.php
@@ -119,11 +119,13 @@ add_action( 'widgets_init', 'krimileksikon_widgets_init' );
 function krimileksikon_scripts() {
 	wp_enqueue_style( 'krimileksikon-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'krimileksikon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'krimileksikon-scripts', get_template_directory_uri() . '/js/krimileksikon.min.js', array(), '20170831', true );
 
-	wp_enqueue_script( 'krimileksikon-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	// wp_enqueue_script( 'krimileksikon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'slick', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array( 'jquery' ), '20151215', true );
+	// wp_enqueue_script( 'krimileksikon-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	// wp_enqueue_script( 'slick', get_template_directory_uri() . '/bower_components/slick-carousel/slick/slick.min.js', array( 'jquery' ), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
